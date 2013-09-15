859aff1d1a92113fb4a18c8f7932daff99890ad8
beonit <beonit@beonit-ultra.(none)>
2013-09-15 23:28:07 +0900

    create config object

--- freemindmap.js
+++ freemindmap.js
@@ -4,9 +4,22 @@ if (window.addEventListener) { // Mozilla, Netscape, Firefox
     window.attachEvent('onload', FreeMindmapLoad);
 }
 
+var CONFIG = (function() {
+    var private = {
+        'background':"#FFFFFF",
+        'textcolor':"#000000",
+        'font':"30px Arial"
+    };
+
+    return {
+        get: function(name) { return private[name]; }
+    };
+})();
+
 function FreeMindmapLoad(event) {
     var canvas = document.getElementById('freemindmap'),
     ctx = canvas.getContext('2d');
+    mapData = {"data":{"left":null , "right":null}}
 
     // resize the canvas to fill browser window dynamically
     window.addEventListener('resize', resizeCanvas, false);
@@ -24,8 +37,13 @@ function FreeMindmapLoad(event) {
     resizeCanvas();
 
     function drawStuff() {
-        console.log("beonit");
-        ctx.fillStyle="#FF0000";
+        // draw background
+        ctx.fillStyle = CONFIG.get('background');
         ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        // draw text
+        ctx.font = CONFIG.get('font');
+        ctx.fillStyle = CONFIG.get('textcolor');
+        ctx.fillText("Hello World",10,50);
     }
 }
