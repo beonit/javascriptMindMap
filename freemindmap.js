5d9529a8d1e0ca098942e7f68e5253a9df0a6ef0
beonit <beonit@beonit-ultra.(none)>
2013-09-16 23:18:46 +0900

    add map container div

--- freemindmap.js
+++ freemindmap.js
@@ -1,39 +1,40 @@
-if (window.addEventListener) { // Mozilla, Netscape, Firefox
-    window.addEventListener('load', FreeMindmapLoad, false);
-} else if (window.attachEvent) { // IE
-    window.attachEvent('onload', FreeMindmapLoad);
-}
-
-function FreeMindmapLoad(event) {
-    var canvas = document.getElementById('freemindmap'),
-    ctx = canvas.getContext('2d');
-    rootX = 100, rootY = 100;
-    config = new MapConfig();    
+function MapController(mapId, containerId, config) {
+    rootX = 0, rootY = 0;
     map = new Map(config);
 
-    // resize the canvas to fill browser window dynamically
-    window.addEventListener('resize', resizeCanvas, false);
+    this.FreeMindmapLoad = function (event) {
+        var canvas = document.getElementById(mapId),
+        container = document.getElementById(containerId),
+        ctx = canvas.getContext('2d');
+        // resize the canvas to fill browser window dynamically
+        window.addEventListener('resize', resizeCanvas, false);
 
-    function resizeCanvas() {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        /**
-         * Your drawings need to be inside this function otherwise they will be reset when 
-         * you resize the browser window and the canvas goes will be cleared.
-         */
-        drawStuff(); 
+        function resizeCanvas() {
+            canvas.width = container.offsetWidth;
+            canvas.height = container.offsetHeight;
+            drawStuff();
+        }
+
+        resizeCanvas();
+
+        function drawStuff() {
+            // draw background
+            ctx.fillStyle = config.get('backgroundColor');
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            // draw text
+            rootX = canvas.width / 2;
+            rootY = canvas.height / 2;
+            map.draw(ctx, rootX, rootY);
+        }
     }
+}
 
-    resizeCanvas();
+config = new MapConfig();
+mapController = new MapController("freemindmap", "mapContainer", config);
 
-    function drawStuff() {
-        // draw background
-        ctx.fillStyle = config.get('backgroundColor');
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-        // draw text
-        rootX = canvas.width / 2;
-        rootY = canvas.height / 2;
-        map.draw(ctx, rootX, rootY);
-    }
+if (window.addEventListener) { // Mozilla, Netscape, Firefox
+    window.addEventListener('load', mapController.FreeMindmapLoad, false);
+} else if (window.attachEvent) { // IE
+    window.attachEvent('onload', mapController.FreeMindmapLoad);
 }
