df7f881f76e097ed4b28d16653f0fd7f4056f4a4
beonit <beonit@gmail.com>
2013-09-21 03:08:06 +0900

    key mapping, but function is not implemented prevent Ctrl-s, Ctrl-o

--- freemindmap.js
+++ freemindmap.js
@@ -2,31 +2,108 @@ function MapController(mapArgs) {
     var rootX = 0, rootY = 0;
     var map = new Map(mapArgs["config"]);
 
-    this.save = function() {
-        jsonStr = map.toJSON();
-        console.log(jsonStr);
-    }
+    var editMode = false;
 
-    this.load = function () {
-    }
-
-    this.add = function() {
+    this.enter = function() { return true; };
+    this.save = function() { return false; };
+    this.load = function() { return false; };
+    this.bold = function() { return true; };
+    this.italic = function() { return true; };
+    this.cut = function() { return true; };
+    this.copy = function() { return true; };
+    this.paste = function() { return true; };
+    this.undo = function() { return true; };
+    this.redo = function() { return true; };
+    this.directRight = function() { return true; };
+    this.directUp = function() { return true; };
+    this.directLeft = function() { return true; };
+    this.directDown = function() { return true; };
+    this.directRight = function() { return true; };
+    this.altLeft = function() { return true; };
+    this.altUp = function() { return true; };
+    this.altRight = function() { return true; };
+    this.altDown = function() { return true; };
+    this.insert = function() {
         map.append({"uname":"owner"});
-    }
-
+        return true;
+    };
     this.delete = function() {
         map.remove({"uname":"owner"});
-    }
+        return true;
+    };
+    this.home = function() { return true; };
+    this.f1 = function() { return true; };
+    this.f2 = function() { return true; };
 
-    this.editMode = function() {
+    var editModeFuncs = {
+        13 : this.enter,
+    };
+
+    var nonEditModeKey = {
+        39 : this.directRight,
+        38 : this.directUp,
+        37 : this.directLeft,
+        40 : this.directDown,
+        45 : this.insert,
+        46 : this.delete,
+        36 : this.home,
+        112 : this.f1,
+        112 : this.f2,
+    };
+
+    var ctrlFuncs = {
+        83 : this.save,
+        79 : this.load,
+        66 : this.bold,
+        73 : this.italic,
+        88 : this.cut,
+        67 : this.copy,
+        86 : this.paste,
+        89 : this.redo,  // z
+        90 : this.undo,  // y
+    };
+
+    var altFuncs = {
+        37 : this.altLeft,
+        38 : this.altUp,
+        39 : this.altRight,
+        40 : this.altDown,
     }
 
     this.FreeMindmapLoad = function (event) {
         var canvas = document.getElementById(mapArgs.mapId),
         container = document.getElementById(mapArgs.containerId),
         ctx = canvas.getContext('2d');
+
         // resize the canvas to fill browser window dynamically
         window.addEventListener('resize', resizeCanvas, false);
+        window.addEventListener("keydown", keydownHandler, false);
+
+        function keydownHandler(e) {
+            keyPropagation = true;
+            if(editMode) {
+                if(editModeFuncs[e.keyCode] != null) {
+                    keyPropagation = editModeFuncs[e.keyCode]();
+                }
+            } else if(e.ctrlKey){
+                if(ctrlFuncs[e.keyCode] != null) {
+                    keyPropagation = ctrlFuncs[e.keyCode]();
+                }
+            } else if(e.altKey){
+                if(altFuncs[e.keyCode] != null) {
+                    keyPropagation = altFuncs[e.keyCode]();
+                }
+            } else if(nonEditModeKey[e.keyCode] != null) {
+                keyPropagation = nonEditModeKey[e.keyCode]();
+            }
+            if(!keyPropagation) {
+                e.preventDefault();
+                e.stopPropagation();
+            }
+            // console.log(e.keyCode);
+
+            return;
+        }
 
         function resizeCanvas() {
             canvas.width = container.offsetWidth;
@@ -53,10 +130,6 @@ mm = new MapController({
     config : CreateMapConfig(),
     mapId : "freemindmap",
     containerId : "mapContainer"});
-mm.add();
-mm.add();
-mm.delete();
-mm.save();
 
 if (window.addEventListener) { // Mozilla, Netscape, Firefox
     window.addEventListener('load', mm.FreeMindmapLoad, false);
